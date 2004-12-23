bd58148ca09bba7f5c2300d5d6d8cee1a8a6c958
schillacia <schillacia>
2004-12-23 07:12:08 +0000

    *** empty log message ***

--- Project/_MAKE_jar.bat
+++ Project/_MAKE_jar.bat
@@ -1,4 +1,4 @@
 javac -O -classpath .;looks-1.2.2.jar jFrame.java
-jar cvfm ../Jif.jar MANIFEST.MF *.class *.properties images/*.gif images/*.png
+jar cvfm ../Jif.jar MANIFEST.MF *.class *.properties images/*.png
 
 pause
