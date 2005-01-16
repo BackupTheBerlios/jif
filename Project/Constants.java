344d4d0d6aa93657ed3ec599bd1d801d52f84f58
schillacia <schillacia>
2005-01-16 20:17:13 +0000

    Updated version number-date

--- Project/Constants.java
+++ Project/Constants.java
@@ -41,7 +41,7 @@ public class Constants {
     /**
      * JIF Version
      */
-    public static final String JIFVERSION = "1.5 Final 20041222";
+    public static final String JIFVERSION = "1.5 Final 20050116";
 
     /**
      * File Separator, depending on which Operative System is executing JIF
