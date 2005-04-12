2451ebf3a2a35ca745e1787568a584999994b18a
schillacia <schillacia>
2005-04-12 11:17:24 +0000

    *** empty log message ***

--- Project/Constants.java
+++ Project/Constants.java
@@ -41,7 +41,7 @@ public class Constants {
     /**
      * JIF Version
      */
-    public static final String JIFVERSION = "1.5.8 20050401";
+    public static final String JIFVERSION = "1.5.9 20050412";
 
     /**
      * File Separator, depending on which Operative System is executing JIF
